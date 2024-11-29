c12d0e03b1b88a3d502773901e57133461310435
Nelson Guamán <nelguaman@gmail.com>
2024-11-29 11:04:54 +0000

    fix: fix format of tender codes

--- src/licitpy/utils/validators.py
+++ src/licitpy/utils/validators.py
@@ -19,7 +19,12 @@ def is_valid_public_market_code(code: str) -> bool:
     """
 
     # Example of a valid code:
+    # - 6-51-O124
+    # - 48-77-LE24
+    # - 885-105-L124
+    # - 1658-1043-L124
     # - 2513-2-LE24
+    # - 5060-452-L124
     # - 750301-54-L124
     # - 1375735-1-L124
 
@@ -31,5 +36,5 @@ def is_valid_public_market_code(code: str) -> bool:
 
     tier_pattern = "|".join([tier.value for tier in Tier])
 
-    pattern = rf"^\d{{4,8}}-\d{{1,2}}-({tier_pattern})\d{{2}}$"
+    pattern = rf"^\d{{1,8}}-\d{{1,4}}-({tier_pattern})\d{{2}}$"
     return bool(re.match(pattern, code))
