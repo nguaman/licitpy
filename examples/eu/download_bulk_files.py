7d8dc499d643b7537445c1004670316a9f19ae61
Nelson Guamán <nelguaman@gmail.com>
2025-07-20 11:53:48 +0100

    feat: Add example script for downloading bulk files from EU

---
+++ examples/eu/download_bulk_files.py
@@ -0,0 +1,20 @@
+import asyncio
+from pprint import pprint
+
+from licitpy.licitpy import Licitpy
+
+
+async def main() -> None:
+    async with Licitpy() as client:
+
+        # Download the monthly bulk file for October 2023
+        file = await client.eu.download_monthly_bulk_file(when="2023-10")
+        pprint(file)
+
+        # Download the entire year 2015
+        files: list[dict] = await client.eu.download_yearly_bulk_file(year="2015")
+        pprint(files)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
